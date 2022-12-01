5c9eff91f01623739315ed35208d99d06237d7ea
israel-1947 <Israeldereje19@gmail.com>
2022-12-01 11:06:17 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,5 @@
---script that creates the table id_not_null on your MySQL server.
-
-CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
+-- Script that creates a table
+-- Query to create the table 'id_not_null' in MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null (
+       id INT DEFAULT 1,
+       name VARCHAR(256));
