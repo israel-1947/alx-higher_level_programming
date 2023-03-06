adaab7fbdbeeae37c7b4479776761ebc6ab374c5
israel-1947 <israeldereje19@gmail.com>
2023-03-06 12:32:02 +0300

    python network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display all HTTP methods the server will accept using curl.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# show all acceptable HTTP methods
+curl -sI -X OPTIONS "$1" | grep Allow | cut -d ' ' -f1 --complement
